c65febb2a461a3df800c6cb8390f1bd9d1dd8a96
Linda Ramos <ltramos7@gmail.com>
2021-02-12 17:13:25 -0600

    feat: added react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import {Route, BrowserRouter as Router} from 'react-router-dom'
+import QuizScreen from './components/QuizScreen/index'
 import HomeScreen from './components/HomeScreen/index'
 
-function App() {
-  return (
-    <div>
-      <HomeScreen/>
-    </div>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Route exact path="/" component={HomeScreen}/>
+          <Route path="/quiz" component={QuizScreen}/>
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+// function App() {
+//   return (
+    
+//       <Router>
+//         <Route exact path="/" component={HomeScreen}/>
+//         <Route path="/quiz" component={QuizScreen}/>
+//       </Router>
+    
+//   );
+// }
+
+// export default App;
