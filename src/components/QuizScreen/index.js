4001caebe878e9bddc4b00066322febf0ddad9c4
Linda Ramos <ltramos7@gmail.com>
2021-02-12 17:07:58 -0600

    feat: created QuizScreen component

---
+++ src/components/QuizScreen/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class QuizScreen extends Component {
+    render() {
+        return (
+            <div>
+                <h1>headline holder</h1>
+            </div>
+        )
+    }
+}
