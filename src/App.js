39f28c59f63b1bae10d8ea69c0fe02b72af8a3dd
Linda Ramos <ltramos7@gmail.com>
2021-02-13 13:36:39 -0600

    feat: added axios and state to hold trivia data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import {Route, BrowserRouter as Router} from 'react-router-dom'
+import axios from 'axios'
 import QuizScreen from './components/QuizScreen/index'
 import HomeScreen from './components/HomeScreen/index'
 
 
+
 export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      triviaData : []
+    }
+  }
+
+  componentDidMount(){
+    axios.get("https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean")
+    .then(resp => this.setState({triviaData: resp.data.results}))
+  }
+
   render() {
+    console.log(this.state.triviaData)
     return (
       <div>
         <Router>
