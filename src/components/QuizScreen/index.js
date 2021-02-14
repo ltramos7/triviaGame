2188433542ad9bfd4903f92a15d421b10e96414c
Linda Ramos <ltramos7@gmail.com>
2021-02-14 12:27:57 -0600

    feat: set randomQuestion into state of QuizScreen Component

--- src/components/QuizScreen/index.js
+++ src/components/QuizScreen/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import QuestionCard from '../QuestionCard';
 
 
 export default class QuizScreen extends Component {
@@ -7,7 +8,8 @@ export default class QuizScreen extends Component {
     constructor(props){
         super(props);
         this.state = {
-          triviaData : []
+            triviaData : [],
+            randomQuestion : []
         }
     }
     
@@ -25,27 +27,14 @@ export default class QuizScreen extends Component {
 
         let randomQuestion = triviaData.splice(randomIndex, 1) // question is also removed from array to prevent duplicate question
 
-        console.log("random question", randomQuestion)
-        console.log("second round", triviaData)
+        this.setState({ randomQuestion: randomQuestion})
     }
 
     render() {
+     
         return (
             <div>
-                <div className="question-category">
-                    <h1>Headline Holder for Question Category</h1>
-                </div>
-                <div className="question-card">
-                    <h1>Holder for Card containing current question</h1>
-                </div>
-                <div className="question-number-tracker">
-                    <h1>1 of 10</h1>
-                </div>
-
-                <div className="answer-options">
-                    <button onClick={this.selectRandomQuestion}>True</button>
-                    <button>False</button>
-                </div>
+                <QuestionCard/>
             </div>
         )
     }
