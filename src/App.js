583bf0a72ca479ee1c9fbe2384eec1dc70b013ff
Linda Ramos <ltramos7@gmail.com>
2021-02-15 16:27:42 -0600

    refactor: moved axios request from app to QuizScreen component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
 import {Route, BrowserRouter as Router} from 'react-router-dom'
+
 import QuizScreen from './components/QuizScreen/index'
 import HomeScreen from './components/HomeScreen/index'
 
 
 
+
 export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+        triviaData: []
+    }
+  }
+
   render() {
     return (
       <div>
         <Router>
           <Route exact path="/" component={HomeScreen}/>
-          <Route path="/quiz" component={QuizScreen}/>
+          <Route path="/quiz" render={() => <QuizScreen triviaData={this.state.triviaData}/>}/>
+          {/* <Route path="/quiz" component={QuizScreen}/> */}
         </Router>
       </div>
     )
--- src/components/QuizScreen/index.js
+++ src/components/QuizScreen/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 import QuestionCard from '../QuestionCard';
+import axios from 'axios'
 
 
 export default class QuizScreen extends Component {
@@ -8,33 +8,36 @@ export default class QuizScreen extends Component {
     constructor(props){
         super(props);
         this.state = {
-            triviaData : [],
-            randomQuestion : []
+            triviaData: [],
+            randomQuestion: {}
         }
     }
-    
-    componentDidMount(){
-        axios.get("https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean")
-        .then(resp => this.setState({triviaData: resp.data.results}))
-    }
 
-    selectRandomQuestion = () => {
-        let triviaData = this.state.triviaData
-        
+    componentDidMount = () => {
+        axios.get("https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean")
+          .then(resp => this.setState({triviaData: resp.data.results, randomQuestion: this.selectRandomQuestion(resp.data.results)}))
+    }
+   
+
+    selectRandomQuestion = (results) => {
+        let triviaData = results
+    
         let questionQuanity = triviaData.length
         
         let randomIndex = Math.floor(Math.random()*questionQuanity)
 
         let randomQuestion = triviaData.splice(randomIndex, 1) // question is also removed from array to prevent duplicate question
 
-        this.setState({ randomQuestion: randomQuestion})
+        return randomQuestion[0]
+    
     }
 
     render() {
-     
+        console.log(this.state)    
         return (
             <div>
-                <QuestionCard/>
+                {/* {this.state.randomQuestion ? <QuestionCard randomQuestion={this.state.randomQuestion} selectRandomQuestion={this.selectRandomQuestion}/> : "Quiz Screen not working"} */}
+                {/* <button onClick={this.selectRandomQuestion}>Test</button> */}
             </div>
         )
     }
