db6230794d393c3d64ad70c03cf0aec6311d3fe9
Linda Ramos <ltramos7@gmail.com>
2021-03-04 12:01:07 -0600

    feat: created results component and connected route in App component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, BrowserRouter as Router} from 'react-router-dom'
 
 import QuizScreen from './components/QuizScreen/index'
 import HomeScreen from './components/HomeScreen/index'
+import ResultsScreen from './components/ResultsScreen'
 
 
 
@@ -13,7 +14,8 @@ export default class App extends Component {
       <div>
         <Router>
           <Route exact path="/" component={HomeScreen}/>
-          <Route path="/quiz" component={QuizScreen}/>
+          <Route exact path="/quiz" component={QuizScreen}/>
+          <Route exact path="/results" component={ResultsScreen}/>
         </Router>
       </div>
     )
--- src/components/ResultsScreen/index.js
+++ src/components/ResultsScreen/index.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+// import { BrowserRouter as Router } from "react-router-dom";
+import { Router } from "react-router-dom";
+import { Route, Redirect } from 'react-router-dom';
+
+
+import HomeScreen from '../HomeScreen'
+
 
 export default class ResultsScreen extends Component {
+
+    returnToHomeScreen = () => {
+        return(
+            <div>
+                <Router>
+                    <Redirect from="/quiz" to="/"/>
+                    <Route exact path="/home" component={HomeScreen}/>
+                </Router>
+            </div>
+        )
+      
+    }
+
     render() {
+        
         return (
             <div>
                 <h1>You scored</h1>
                 <h3>"x"/10</h3>
-                {/* use map to render incorrect or correct answer result cards */}
-                <button>PLAY AGAIN?</button>
+                {/* use map to render incorrect or correct answer result cards */}                  
+
+                {/* <button onClick={this.returnToHomeScreen}>PLAY AGAIN?</button> */}
+                {/* <button onClick={()=>{this.props.history.push('/home')}}>PLAY AGAIN?</button> */}
+                <Link to='/home'>
+                    <button>Begin</button>
+                </Link>
+                
             </div>
         )
     }
